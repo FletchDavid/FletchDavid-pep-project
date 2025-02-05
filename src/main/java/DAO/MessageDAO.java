1fe6b4e8e82327b3ab90621e8b21a6eeff8d1108
David Fletcher <drfletch17@gmail.com>
2025-02-05 05:12:20 +0000

    Implemented Message DAO and Services

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -6,53 +6,172 @@ import Util.ConnectionUtil;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.DelayQueue;
 
 public class MessageDAO {
     /*
      * Requirement 3: Posting a new message
      */
     public Message postMessage(Message message){
-        //TODO
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO Message(posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()){
+                int generatedID = (int) resultSet.getLong(1);
+                return new Message(generatedID, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
+    }
+
+    public boolean accountIDExists(int id){
+        Connection connection = ConnectionUtil.getConnection();
+        
+        try{
+            String sql = "SELECT * FROM Account WHERE account_id=?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 
     /*
      * Requirement 4: Getting all messages
      */
     public List<Message> getAllMessages(){
-        //TODO
-        return null;
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
+        try{
+            String sql = "SELECT * FROM Message";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Message message = new Message(
+                    resultSet.getInt("message_id"),
+                    resultSet.getInt("posted_by"),
+                    resultSet.getString("message_text"),
+                    resultSet.getLong("time_posted_epoch")
+                );
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
     }
 
     /*
      * Requirement 5: Get messages via their ID
      */
     public Message getMessageByID(int id){
-        //TODO
+        Connection connection = ConnectionUtil.getConnection();
+
+        try{
+            String sql = "SELECT * FROM Message WHERE message_id=?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                Message message = new Message(
+                    id,
+                    resultSet.getInt("posted_by"),
+                    resultSet.getString("message_text"),
+                    resultSet.getLong("time_posted_epoch")
+                );
+                return message;
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     /*
      * Requirement 6: Delete Message using its ID 
      */
-    public Message deleteMessageByID(int id){
-        //TODO
-        return null;
+    public void deleteMessageByID(int id){
+        Connection connection = ConnectionUtil.getConnection();
+
+        try{
+            String sql = "DELETE FROM Message WHERE message_id=?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeQuery();
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     /*
      * Requirement 7: Update message by ID
      */
-    public Message updateMessageByID(Message message){
-        //TODO
-        return null;
+    public boolean updateMessageByID(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        int updated = 0;
+        try{
+            String sql = "UPDATE Message SET message_text=? WHERE message_id=?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, message.getMessage_text());
+            preparedStatement.setInt(2, message.getMessage_id());
+
+            updated = preparedStatement.executeUpdate();
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        if(updated>0) return true;
+        else return false;
     }
 
     /*
      * Requirement 8: Get all messages by a user by its User ID
      */
     public List<Message> getMessagesByUser(int id){
-        //TODO
-        return null;
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages= new ArrayList<>();
+
+        try{
+            String sql = "SELECT * FROM Message WHERE posted_by=?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Message message = new Message(
+                    resultSet.getInt("message_id"),
+                    resultSet.getInt("posted_by"),
+                    resultSet.getString("message_text"),
+                    resultSet.getLong("time_posted_epoch")
+                );
+                messages.add(message);
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
     }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -20,39 +20,45 @@ public class MessageService {
      * Requirement 3: Posting a new message
      */
     public Message postMessage(Message message){
-        //TODO
-        return null;
+        if(0 < message.getMessage_text().length() && message.getMessage_text().length() <= 255 && messageDAO.accountIDExists(message.getPosted_by())){
+            return messageDAO.postMessage(message);
+        }else return null;
     }
 
     /*
      * Requirement 4: Getting all messages
      */
     public List<Message> getAllMessages(){
-        //TODO
-        return null;
+        return messageDAO.getAllMessages();
     }
 
     /*
      * Requirement 5: Get messages via their ID
      */
     public Message getMessageByID(int id){
-        //TODO
-        return null;
+        return messageDAO.getMessageByID(id);
     }
 
     /*
      * Requirement 6: Delete Message using its ID 
      */
     public Message deleteMessageByID(int id){
-        //TODO
-        return null;
+        Message message = messageDAO.getMessageByID(id);
+        messageDAO.deleteMessageByID(id);
+        return message;
     }
 
     /*
      * Requirement 7: Update message by ID
      */
     public Message updateMessageByID(Message message){
-        //TODO
+        if(message.getMessage_text().length()>255 || message.getMessage_text() == "") return null;
+        else if(messageDAO.getMessageByID(message.getMessage_id())!=null){
+            //try to update the database. return the message if successful and return null if not.
+            if(messageDAO.updateMessageByID(message)){
+                return messageDAO.getMessageByID(message.getMessage_id());
+            }
+        }
         return null;
     }
 
@@ -60,7 +66,6 @@ public class MessageService {
      * Requirement 8: Get all messages by a user by its User ID
      */
     public List<Message> getMessagesByUser(int id){
-        //TODO
-        return null;
+        return messageDAO.getMessagesByUser(id);
     }
 }
